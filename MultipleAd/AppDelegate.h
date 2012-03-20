3fec347e73ea4caafd6e13e5ce19242a24887b01
Anurag Solanki <anurag@vinsol.com>
2012-03-20 10:01:13 +0530

    Initial Commit

---
+++ MultipleAd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultipleAd
+//
+//  Created by Anurag Solanki on 20/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MultipleAd/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultipleAd
+//
+//  Created by Anurag Solanki on 20/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
